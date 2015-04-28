890394d7e9175b2ca804ea53e027c86f35f80847
ravonyx <ravonyxa@gmail.com>
2015-04-28 22:56:15 +0200

    can choose size / margin / % of type building

--- city_gen.py
+++ city_gen.py
@@ -19,18 +19,36 @@ from math import *
  
 #properties of the panel
 def initSceneProperties(scn):
-	bpy.types.Scene.Length = IntProperty(
-		name = "Length", 
+	bpy.types.Scene.Margin = IntProperty(
+		name = "Margin", 
 		description = "Enter an integer",
-		default = 6,
+		default = 10,
 		min = 5,
-		max = 8)
+		max = 30)
 	bpy.types.Scene.Taille = IntProperty(
 		name = "Taille", 
 		description = "Enter an integer",
-		default = 3,
-		min = 1,
-		max = 6)
+		default = 170,
+		min = 50,
+		max = 200)
+	bpy.types.Scene.ValueHouse = FloatProperty(
+		name = "% House", 
+		description = "Enter an float",
+		default = 0.2,
+		min = 0,
+		max = 1)
+	bpy.types.Scene.ValueLittle = FloatProperty(
+		name = "% Little Building", 
+		description = "Enter an float",
+		default = 0.2,
+		min = 0,
+		max = 1)
+	bpy.types.Scene.ValueBig = FloatProperty(
+		name = "% Big Building", 
+		description = "Enter an float",
+		default = 0.2,
+		min = 0,
+		max = 1)
 	return
 
 initSceneProperties(bpy.context.scene)
@@ -46,8 +64,16 @@ class ToolsPanel(bpy.types.Panel):
 		
 		layout.label("Parameters")
 		row = layout.row(align=True)
-		row.alignment = 'EXPAND'
-		row.prop(scn, 'Taille', text = "taille")
+		row = layout.row()
+		split = row.split(percentage=0.8)
+		col = split.column()
+		col.prop(scn, 'Taille', text = "taille")
+		col.prop(scn, 'Margin', text = "margin")
+		split = row.split(percentage=1)
+		col = split.column()
+		col.prop(scn, 'ValueHouse', text = "% house")
+		col.prop(scn, 'ValueLittle', text = "% little building")
+		col.prop(scn, 'ValueBig', text = "% big building")
 		
 		layout.label("Tools")
 		row = layout.row()
@@ -70,9 +96,12 @@ class OBJECT_OT_GenButton(bpy.types.Operator):
 	bl_idname = "town.gen"
 	bl_label = "Create town"
 	def execute(self, context):
-		lenght = bpy.context.scene.Length
+		margin = bpy.context.scene.Margin
 		taille = bpy.context.scene.Taille
-		generate_town(taille)
+		valueHouse = bpy.context.scene.ValueHouse
+		valueLittle = bpy.context.scene.ValueLittle
+		valueBig = bpy.context.scene.ValueBig
+		generate_town(taille, margin, valueHouse, valueLittle, valueBig)
 		return{'FINISHED'}    
 
 class OBJECT_OT_DeleteButton(bpy.types.Operator):
@@ -102,23 +131,25 @@ class OBJECT_OT_ExportButton(bpy.types.Operator):
 bpy.utils.register_module(__name__)
 
 
-def generate_town(taille):
-	taille = 170
+def generate_town(taille, margeBetweenBat, valueHouse, valueLittle, valueBig):
 	margin = 5
-	margeBetweenBat = 10
+	
 	#CREATE BUILDINGS
 	#SET FLOOR
 	bpy.ops.mesh.primitive_plane_add(location = (0, 0, 0))
 	bpy.ops.transform.resize(value=(taille, taille, 0)) 
 
 	#BUILDINGS POSITIONS
-	nbBat = 1156
-	buildingPosTab = np.zeros((nbBat, 2), dtype='i')
 	x=- (taille - margin)
 	y=- (taille - margin)
+	nbUnitPerCote = (((taille - margin) * 2) / margeBetweenBat) + 1;
+	
+	nbBat = nbUnitPerCote * nbUnitPerCote;
+	buildingPosTab = np.zeros((nbBat, 2), dtype='i')
+	
 	i=0
-	for posNumX in range(0,34):
-		for posNumY in range(0,34): 
+	for posNumX in range(0, int(nbBat/nbUnitPerCote)):
+		for posNumY in range(0,int(nbBat/nbUnitPerCote)): 
 			print(posNumX)
 			print(posNumY)                         
 			buildingPosTab[i][0] = x
@@ -130,7 +161,11 @@ def generate_town(taille):
 	print(buildingPosTab)
 
 	i = 0
-	#while i != 0:
+	
+	rangeHouse = taille - taille * valueHouse
+	rangeLittleBuilding = taille - taille * (valueHouse + valueLittle)
+	rangeBigBuilding = taille - taille * (valueHouse + valueLittle + valueBig)
+
 	for buildNum in range(0,len(buildingPosTab)):
 		#randPositionX = uniform(-50,50) 
 		#randPositionY = uniform(-50,50) 
@@ -138,16 +173,16 @@ def generate_town(taille):
 		positionY = buildingPosTab[i][1]
 		#Centralisation des populations
 		randBatiment = randint(0, 3)
-		if positionX >= -45 and positionX <= 45 and positionY >= -45 and positionY <= 45:       
+		if positionX >= -rangeBigBuilding and positionX <= rangeBigBuilding and positionY >= -rangeBigBuilding and positionY <= rangeBigBuilding:       
 			#if randBatiment == 0:
 			createTower(positionX, positionY) 
-		elif (positionX >= -95 and positionX <= -55 and positionY >= -95 and positionY <= 95) or (positionX >= 55 and positionX <= 95 and positionY >= -95 and positionY <= 95) or (positionX >= -95 and positionX <= 95 and positionY >= -95 and positionY <= -55) or (positionX >= -95 and positionX <= 95 and positionY >= 55 and positionY <= 95) :
+		elif (positionX >= -rangeLittleBuilding and positionX <= -rangeBigBuilding and positionY >= -rangeLittleBuilding and positionY <= rangeLittleBuilding) or (positionX >= rangeBigBuilding and positionX <= rangeLittleBuilding and positionY >= -rangeLittleBuilding and positionY <= rangeLittleBuilding) or (positionX >= -rangeLittleBuilding and positionX <= rangeLittleBuilding and positionY >= -rangeLittleBuilding and positionY <= -rangeBigBuilding) or (positionX >= -rangeLittleBuilding and positionX <= rangeLittleBuilding and positionY >= rangeBigBuilding and positionY <= rangeLittleBuilding) :
 			#elif randBatiment == 1:
 				createBigBuilding(positionX, positionY) 
-		elif (positionX >= -135 and positionX <= -105 and positionY >= -135 and positionY <= 135) or (positionX >= 105 and positionX <= 135 and positionY >= -135 and positionY <= 135) or (positionX >= -135 and positionX <= 135 and positionY >= -135 and positionY <= -105) or (positionX >= -135 and positionX <= 135 and positionY >= 105 and positionY <= 135):
+		elif (positionX >= -rangeHouse and positionX <= -rangeLittleBuilding and positionY >= -rangeHouse and positionY <= rangeHouse) or (positionX >= rangeLittleBuilding and positionX <= rangeHouse and positionY >= -rangeHouse and positionY <= rangeHouse) or (positionX >= -rangeHouse and positionX <= rangeHouse and positionY >= -rangeHouse and positionY <= -rangeLittleBuilding) or (positionX >= -rangeHouse and positionX <= rangeHouse and positionY >= rangeLittleBuilding and positionY <= rangeHouse):
 			#elif randBatiment == 2:
 				createLittleBuilding(positionX, positionY)
-		elif positionX <= -145 or positionX >= 145 or positionY <= -145 or positionY >= 145:
+		elif positionX <= -rangeHouse or positionX >= rangeHouse or positionY <= -rangeHouse or positionY >= rangeHouse:
 		   # elif randBatiment == 3:
 			createHouse(positionX, positionY)
 		"""else:
