a8c16fecf63b0ca5716e6e47a0ea55ea2d23a4a6
ravonyx <ravonyxa@gmail.com>
2015-06-14 23:14:07 +0200

    add texture

--- city_gen.py
+++ city_gen.py
@@ -108,8 +108,9 @@ class OBJECT_OT_DeleteButton(bpy.types.Operator):
 	bl_idname = "town.delete"
 	bl_label = "Delete town"
 	def execute(self, context):
+		bpy.ops.object.mode_set(mode='OBJECT')
 		for ob in bpy.context.scene.objects:
-			ob.select = (ob.type == 'MESH' or ob.type == 'LAMP') and (ob.name.startswith("Cylinder") or ob.name.startswith("Cube") or ob.name.startswith("Plane"))
+			ob.select = (ob.type == 'MESH' or ob.type == 'LAMP') and (ob.name.startswith("Cylinder") or ob.name.startswith("House") or ob.name.startswith("Little") or ob.name.startswith("Tower") or ob.name.startswith("Big")  or ob.name.startswith("Cube") or ob.name.startswith("Plane"))
 			bpy.ops.object.delete()
 		return{'FINISHED'}    
 
@@ -213,66 +214,139 @@ def generate_town(taille, margeBetweenBat, valueHouse, valueLittle, valueBig):
 	"""
 
 def createHouse(posX, posY):  
-    cote = uniform(2,4)
-    hauteur = cote#(cote*2)/3
-    posZ = hauteur
-    bpy.ops.mesh.primitive_cube_add(location = (posX, posY, posZ))  
-    bpy.ops.transform.resize(value=(cote, cote, hauteur)) 
-    #SET COLOR GREEN
-    item = bpy.context.object
-    #setMaterial('green', item)
-    
+	cote = uniform(4, 6)
+	hauteur = cote * 2#(cote*2)/3
+	mesh, object = create_building('House', posX, posY,  hauteur, cote)
+
+	object = bpy.context.active_object 
+	obj_data = object.data
+	bpy.ops.object.mode_set(mode='EDIT')
+	mesh = bmesh.from_edit_mesh(obj_data)
+	
+	nbText = randint(1,4)
+	nameMat = 'tower' + str(nbText) + '.jpg'
+	set_uv(nameMat, object, mesh,2,2)
+	
+	bpy.ops.object.mode_set(mode='OBJECT')
+		
+
 def createLittleBuilding(posX, posY):
-    cote = uniform(2,4)
-    hauteur = cote*3
-    posZ = hauteur
-    bpy.ops.mesh.primitive_cube_add(location = (posX, posY, posZ))  
-    bpy.ops.transform.resize(value=(cote, cote, hauteur)) 
-    #SET COLOR pink 
-    item = bpy.context.object
-    #setMaterial('pink', item)    
+	cote = uniform(4,6)
+	hauteur = cote*4
+	create_building('Little Building', posX, posY,  hauteur, cote)
+	
+	object = bpy.context.active_object 
+	obj_data = object.data
+	bpy.ops.object.mode_set(mode='EDIT')
+	mesh = bmesh.from_edit_mesh(obj_data)
+	
+	nbText = randint(1,4)
+	nameMat = 'big' + str(nbText) + '.jpg'
+	set_uv(nameMat, object, mesh,2,4)
+	
+	bpy.ops.object.mode_set(mode='OBJECT')
     
 def createBigBuilding(posX, posY):
-    cote = uniform(2,4)
-    hauteur = cote*5
-    posZ = hauteur
-    bpy.ops.mesh.primitive_cube_add(location = (posX, posY, posZ))  
-    bpy.ops.transform.resize(value=(cote, cote, hauteur)) 
-    #SET COLOR BLUE 
-    item = bpy.context.object
-    #setMaterial('blue', item)
+	cote = uniform(4,6)
+	hauteur = cote*6
+	create_building('Big Building', posX, posY,  hauteur, cote)
     
+	object = bpy.context.active_object 
+	obj_data = object.data
+	bpy.ops.object.mode_set(mode='EDIT')
+	mesh = bmesh.from_edit_mesh(obj_data)
+	
+	nbText = randint(1,4)
+	nameMat = 'big' + str(nbText) + '.jpg'
+	set_uv(nameMat, object, mesh,2,5)
+	
+	bpy.ops.object.mode_set(mode='OBJECT')
 def createTower(posX, posY):
-    cote = uniform(2,4)
-    hauteur = cote*10
-    posZ = hauteur
-    bpy.ops.mesh.primitive_cube_add(location = (posX, posY, posZ))  
-    bpy.ops.transform.resize(value=(cote, cote, hauteur))
-    #SET COLOR RED
-    item = bpy.context.object   
-    #setMaterial('red', item)
-    
-def setMaterial(color, item):
-    mat = bpy.data.materials.new(color)
-    if color == 'red':
-        mat.diffuse_color = (1,0,0)
-    elif color == 'green':
-        mat.diffuse_color = (0,1,0)
-    elif color =='blue':
-        mat.diffuse_color = (0,0,1)
-    elif color =='pink':
-        mat.diffuse_color = (1,0,1)
-    #mat.diffuse_shader = 'LAMBERT' 
-    #mat.diffuse_intensity = 1.0 
-    #mat.specular_color = (1,1,1)
-    #mat.specular_shader = 'COOKTORR'
-    #mat.specular_intensity = 0.5
-    #mat.alpha = 1
-    #mat.ambient = 1
-    item.data.materials.append(mat)
-    
-#_________________________________________________#
+	cote = uniform(4,6)
+	hauteur = cote*10
+	create_building('Tower', posX, posY,  hauteur, cote)
+	
+	object = bpy.context.active_object 
+	obj_data = object.data
+	bpy.ops.object.mode_set(mode='EDIT')
+	mesh = bmesh.from_edit_mesh(obj_data)
+	
+	nbText = randint(1,4)
+	nameMat = 'tower' + str(nbText) + '.jpg'
+	set_uv(nameMat, object, mesh,2,6)
+	
+	bpy.ops.object.mode_set(mode='OBJECT')
 
+def create_cube_coords(coords,px, py, pz, hauteur, largeur):
+	coords.append(( px, py, pz ))
+	coords.append(( px + largeur, py, pz ))
+	coords.append(( px + largeur, py + largeur, pz ))
+	coords.append(( px, py+largeur , pz ))
+	coords.append(( px, py, pz + hauteur))
+	coords.append(( px + largeur, py, pz + hauteur))
+	coords.append(( px + largeur, py + largeur, pz + hauteur))
+	coords.append(( px, py+largeur , pz + hauteur))
+
+def create_cube_faces(faces, idx):
+	faces.append(( idx, idx+1, idx+2, idx+3 ))
+	faces.append(( idx, idx+1, idx+5, idx+4 ))
+	faces.append(( idx+1, idx+2, idx+6, idx+5 ))
+	faces.append(( idx+2, idx+3, idx+7, idx+6 ))
+	faces.append(( idx+3, idx, idx+4, idx+7 ))
+	faces.append(( idx+4, idx+5, idx+6, idx+7 ))
+	
+def create_building(name, px, py, hauteur, largeur):
+	bpy.ops.object.select_all(action='TOGGLE')
+
+	mesh = bpy.data.meshes.new(name="Mesh")
+	coords = []
+	faces = []
+	pz = 0
+	idx = 0
+	
+	create_cube_coords(coords,px, py, pz, hauteur, largeur)
+	create_cube_faces(faces, idx)
+
+	object = bpy.data.objects.new( name, mesh )
+	#link de l'object à la scene
+	bpy.context.scene.objects.link( object )
+	mesh.from_pydata( coords, [], faces )
+	mesh.update( calc_edges=True )
+	bpy.context.scene.objects.active = object
+	object.select = True
+	
+	return mesh, object
+
+
+def set_uv(nameMat, object, mesh, scalex, scaley):
+	mat = createMaterial('img/' + nameMat)
+	mat.specular_intensity = 0
+	mat.texture_slots[0].texture_coords = 'UV'
+	mat.texture_slots[0].scale[0] = scalex
+	mat.texture_slots[0].scale[1] = scaley
+	setMaterial(object, mat)
+	
+	mesh.faces[1].select = True	
+	mesh.faces[2].select = True	
+	mesh.faces[3].select = True	
+	mesh.faces[4].select = True	
+	bpy.ops.uv.unwrap(method='ANGLE_BASED', margin=0.001)
+    
+def createMaterial(name):
+	img = bpy.data.images.load('//'+name)
+	tex = bpy.data.textures.new('TexName', type = 'IMAGE')
+	tex.image = img
+	mat = bpy.data.materials.new('MatName')
+	
+	ctex = mat.texture_slots.add()
+	ctex.texture = tex
+	ctex.texture_coords = 'UV'
+	ctex.mapping = 'CUBE'
+	return mat
+	
+def setMaterial(ob, mat):
+    me = ob.data
+    me.materials.append(mat)
 
 
 
